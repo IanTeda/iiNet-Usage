fef7375ec449d48a279fb1e4327940e131af762c
Ian Teda <ian@teda.id.au>
2012-01-02 08:25:03 +1100

    Added some functionality to actionbar

--- iiNet Usage Phone/src/au/id/teda/iinetusage/phone/activity/ActionbarActivity.java
+++ iiNet Usage Phone/src/au/id/teda/iinetusage/phone/activity/ActionbarActivity.java
@@ -2,7 +2,11 @@ package au.id.teda.iinetusage.phone.activity;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import au.id.teda.iinetusage.phone.R;
 
 public class ActionbarActivity extends Activity {
 	
@@ -12,6 +16,25 @@ public class ActionbarActivity extends Activity {
 	
 	public void onClickActionbarRefresh (View view){
 		Toast.makeText(this, "Refersh data", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void hideActionbarHomeIcon(){
+		ImageButton myHomeImageButton = (ImageButton) findViewById(R.id.actionbar_home_button);
+		ImageView mySeparatorImage = (ImageView) findViewById(R.id.actionbar_separator_1);
+		myHomeImageButton.setVisibility(View.GONE);
+		mySeparatorImage.setVisibility(View.GONE);
+	}
+	
+	public void showActionbarHomeIcon(){
+		ImageButton myHomeImageButton = (ImageButton) findViewById(R.id.actionbar_home_button);
+		ImageView mySeparatorImage = (ImageView) findViewById(R.id.actionbar_separator_1);
+		myHomeImageButton.setVisibility(View.VISIBLE);
+		mySeparatorImage.setVisibility(View.VISIBLE);
+	}
+	
+	public void setActionbarTitle(String actionbarTitle){
+		TextView myActionbarTitle = (TextView) findViewById(R.id.actionbar_title);
+		myActionbarTitle.setText(actionbarTitle);
 	}
 
 }
